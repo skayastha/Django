c27e11fa975522b55c3c80b4714a1f39be4b6bc5
skayastha <summitkayastha@hotmail.com>
2024-06-25 22:58:25 -0500

    Adding new apps to the project

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -4,17 +4,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
-
+    return render(request, "hello/index.html")
 
 def newPage(request):
     return HttpResponse("Welcome to New Page!")
-
 
 def summit(request):
     return HttpResponse("Hello Summit!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello {name}")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
+
 
 
