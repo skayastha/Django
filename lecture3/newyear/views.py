ec635223e814ec55a86cfe34e8b8764fe0f2e56a
skayastha <summitkayastha@hotmail.com>
2024-06-26 22:36:33 -0500

    Adding tasks

--- lecture3/newyear/views.py
+++ lecture3/newyear/views.py
@@ -7,5 +7,6 @@ import datetime
 def index(request):
     now = datetime.datetime.now()
     return render(request, "newyear/index.html", {
-        "newyear" : now.month == 1 and now.day == 1 
+        "newyear" : True
+        
     })
