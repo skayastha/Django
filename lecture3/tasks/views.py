ab8fbc2dc107da344b665b964075ee841fc60c4d
skayastha <summitkayastha@hotmail.com>
2024-06-26 22:52:49 -0500

    Final django commit

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render
 from django.urls import reverse
 from django import forms
 
-tasks = []
-
 class NewTaskForm(forms.Form):
     task = forms.CharField(label = "New Task")
     priority = forms.IntegerField(label="Priority", min_value= 1, max_value=10)
@@ -12,8 +10,10 @@ class NewTaskForm(forms.Form):
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks" : tasks
+        "tasks" : request.session["tasks"]
     })
 
 def add(request):
@@ -22,7 +22,7 @@ def add(request):
 
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
